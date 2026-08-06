0487541a7393d54afc47d25da5686a0c53f84f91
kyuubi-byte <3125022941@qq.com>
2026-08-06 11:27:37 +0800

    2-5 aiapinode 提交

--- ai-agent-scaffold-domain/src/main/java/org/example/domain/agent/service/armory/factory/DefaultArmoryFactory.java
+++ ai-agent-scaffold-domain/src/main/java/org/example/domain/agent/service/armory/factory/DefaultArmoryFactory.java
@@ -1,26 +1,41 @@
 package org.example.domain.agent.service.armory.factory;
 
+import cn.bugstack.wrench.design.framework.tree.StrategyHandler;
+import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.domain.agent.model.entity.ArmoryCommandEntity;
+import org.example.domain.agent.model.valobj.AiAgentRegisterVO;
+import org.example.domain.agent.service.armory.node.RootNode;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.ai.openai.api.OpenAiApi;
 
 import java.util.HashMap;
 import java.util.Map;
 @Service
 public class DefaultArmoryFactory {
+
+    @Resource
+    private RootNode rootNode;
+    public StrategyHandler<ArmoryCommandEntity,DynamicContext, AiAgentRegisterVO>armoryStrategyHandler(){
+        return rootNode;
+    }
+
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class DynamicContext{
+
+        private OpenAiApi openAiApi;
+
         private Map<String,Object>dataObjects=new HashMap<>();
-        public <T>void setValue(String key,T value){
-            dataObjects.put(key,value);
-        }
-        public <T> T getValue(String key){
-            return (T)dataObjects.get(key);
-        }
+
+        public <T>void setValue(String key,T value){dataObjects.put(key,value);}
+
+        public <T> T getValue(String key){return (T)dataObjects.get(key);}
+
     }
 }
--- ai-agent-scaffold-domain/src/main/java/org/example/domain/agent/service/armory/node/AiApiNode.java
+++ ai-agent-scaffold-domain/src/main/java/org/example/domain/agent/service/armory/node/AiApiNode.java
@@ -2,11 +2,14 @@ package org.example.domain.agent.service.armory.node;
 
 import cn.bugstack.wrench.design.framework.tree.StrategyHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.example.domain.agent.model.entity.ArmoryCommandEntity;
+import org.example.domain.agent.model.valobj.AIAgentConfigTableVO;
 import org.example.domain.agent.model.valobj.AiAgentRegisterVO;
 import org.example.domain.agent.service.armory.AbstractArmorySupport;
 import org.example.domain.agent.service.armory.factory.DefaultArmoryFactory;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.ai.openai.api.OpenAiApi;
 
 @Slf4j
 @Service
@@ -14,8 +17,17 @@ public class AiApiNode extends AbstractArmorySupport {
 
     @Override
     protected AiAgentRegisterVO doApply(ArmoryCommandEntity requestParameter, DefaultArmoryFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("Ai Agent 装配操作 - AiApiNode");
+        AIAgentConfigTableVO aiAgentConfigTableVO = requestParameter.getAiAgentConfigTableVO();
+        AIAgentConfigTableVO.Module.AiApi aiApiConfig = aiAgentConfigTableVO.getModule().getAiApi();
 
-        //编写api实例化操作
+        OpenAiApi openAiApi = OpenAiApi.builder()
+                .baseUrl(aiApiConfig.getBaseUrl())
+                .apiKey(aiApiConfig.getApiKey())
+                .completionsPath(StringUtils.isNoneBlank(aiApiConfig.getCompletionsPath())?aiApiConfig.getCompletionsPath():"v1/chat/completions")
+                .embeddingsPath(StringUtils.isNoneBlank(aiApiConfig.getEmbeddingsPath())?aiApiConfig.getEmbeddingsPath():"v1/embeddings")
+                .build();
+        dynamicContext.setOpenAiApi(openAiApi);
         return router(requestParameter,dynamicContext);
     }
 
